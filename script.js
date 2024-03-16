d16052678696db25cc957b7067c1f133efd8900c
root <132725436+tingtung46@users.noreply.github.com>
2024-03-16 19:22:17 +0700

    Add main logic for buttons (close, append book)

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const dialog = document.querySelector('#book-popup');
 const addBook = document.querySelector('.adding-book');
 const navAddBook = document.querySelector('#nav-append')
 const closeBtn = document.querySelector('#cancel');
+const appendBook = document.querySelector('#append-book');
+const bookContainer = document.querySelector('.hero');
 
 addBook.addEventListener('click', () => {
     dialog.showModal();
@@ -14,3 +16,59 @@ navAddBook.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     dialog.close();
 });
+
+appendBook.addEventListener('submit', addBookToLibrary);
+
+const titleInput = document.querySelector('#book-title');
+const authorInput = document.querySelector('#book-author');
+const pageNumberInput = document.querySelector('#page-number');
+const readInput = document.querySelector('#read');
+
+const library = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = pages;
+    this.readingStatus = read;
+};
+
+function addBookToLibrary(e) {
+    const newBook = new Book(
+        titleInput.value, 
+        authorInput.value, 
+        pageNumberInput.value, 
+        readInput.checked
+    );
+
+    library.push(newBook);
+
+    e.preventDefault();
+    dialog.close();
+};
+
+function appendNewBook() {
+    const book = document.createElement('div');
+    
+    bookContainer.appendChild(book);
+    book.setAttribute('id', `${library.length}`);
+    book.setAttribute('class', 'book');
+
+    const title = document.createElement('h3');
+    const author = document.createElement('p');
+    const pageNumber = document.createElement('p');
+    const read = document.createElement('label');
+    const checkbox = document.createElement('checkbox');
+    const slider = document.createElement('span');
+    const remove = document.createElement('button');
+
+    read.append(checkbox, slider);
+    read.setAttribute('id', 'read');
+    remove.setAttribute('id', 'remove');
+
+    book.appendChild(title);
+    book.appendChild(author);
+    book.appendChild(pageNumber);
+    book.appendChild(read);
+    book.appendChild(remove);
+};
